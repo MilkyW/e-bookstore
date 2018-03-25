6e12c3dd9659915e50a6246a5bc8fba9e78c3e41
Milky.W <minniewu@qq.com>
2018-03-25 20:14:21 +0800

    completed nav css

--- src/App.js
+++ src/App.js
@@ -1,43 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import {Button, ButtonToolbar, Nav, Navbar, NavItem, NavDropdown, MenuItem, FormGroup, FormControl} from 'react-bootstrap';
+import { Button, ButtonToolbar, Table, Nav, Navbar, NavItem, NavDropdown, DropdownButton, MenuItem, Form, FormGroup, FormControl } from 'react-bootstrap';
 
-const navbarInstance = (
-  <Navbar inverse collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#">React-Bootstrap</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <NavItem eventKey={1} href="#">Link</NavItem>
-        <NavItem eventKey={2} href="#">Link</NavItem>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={3.3}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
-      <Nav pullRight>
-        <NavItem eventKey={1} href="#">Link Right</NavItem>
-        <NavItem eventKey={2} href="#">Link Right</NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
+
+const navbar = {backgroundColor: 'rgb(0, 0, 0)', borderBottomWidth:"3px" ,borderColor: '#5B6066'};
+
+class PageNav extends Component {
+  handleSelect(selectedKey) {
+  }
+
+  render() {
+    return (
+      <div>
+          <Nav bsStyle="tabs" justified activeKey={1} onSelect={this.handleSelect}>
+            <NavItem style={navbar} eventKey={1} href="/home">Home</NavItem>
+            <NavItem style={navbar} eventKey={2} title="Item">My Account</NavItem>
+            <NavItem style={navbar} eventKey={3} >Shopping Cart</NavItem>
+            <NavItem style={navbar} eventKey={4} >Sign up</NavItem>
+          </Nav>
+      </div>
+    );
+  }
+}
+
+class MyHeader extends Component {
+  render() {
+    return (
+      <div className="MyHeader">
+        <Table id="headerbg" cellspacing={0} cellpadding={0} width={745} border={0} align="center">
+          <tr width={745}><td height={120}></td></tr>
+          <tr width={745}><div id="pn"><PageNav className="mypn" /></div></tr>
+          <tr width={745}>
+            <td height={70}>
+              <Form componentClass="fieldset" inline justified>
+                <FormGroup bsSize="small">
+                  <FormControl id="se0" type="text" placeholder="From" />
+                  <FormControl id="se0" type="text" placeholder="To" />
+                  <FormControl id="se1" type="text" placeholder="Keyword" />
+                </FormGroup>
+              </Form>
+            </td>
+          </tr>
+        </Table >
+      </div >
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>{navbarInstance}</div>
+        <MyHeader />
+        <div>{}</div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
