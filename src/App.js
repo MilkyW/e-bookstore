f693a6764fbef010e55c28812d25371a8c320b59
Milky.W <minniewu@qq.com>
2018-03-25 21:49:13 +0800

    complete static excel

--- src/App.js
+++ src/App.js
@@ -5,8 +5,51 @@ import '../node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
 import './App.css';
 import { Button, ButtonToolbar, Table, Nav, Navbar, NavItem, NavDropdown, DropdownButton, MenuItem, Form, FormGroup, FormControl } from 'react-bootstrap';
 
+const headers = [
+  "Book", "Author", "Language", "Published", "Sales"
+];
 
-const navbar = {backgroundColor: 'rgb(0, 0, 0)', borderBottomWidth:"3px" ,borderColor: '#5B6066'};
+const data = [
+  ["The Lord of the Rings", "J.R.R.Tolkien", "English", "1954-1955", "150 million"],
+  ["Le Petit Prince (The Little Prince)", "Antoine de Saint-Exupery", "French", "1943", "140 million"],
+  ["Harry Potter and the Philosopher's Stone", "J.K.Rowling", "English", "1997", "107 millon"],
+  ["And Then There Were None", "Agatha Christie", "English", "1939", "100 million"],
+  ["Dream of the Red Chamber", "Cao Xueqin", "Chinese", "1754-1791", "100 million"],
+  ["The Hobbit", "J.R.R.Tolkien", "English", "1937", "100 million"],
+  ["She: A History of Adventure", "H.Rider Haggard", "English", "1887", "100 million"]
+];
+
+class Excel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: this.props.initialData
+    };
+  }
+
+  render() {
+    return (
+      <div id="table">
+        <Table striped bordered condensed hover>
+          <thead>
+            <tr>
+              {this.props.headers.map(function (title, idx) { return <th id="thh" key={idx}>{title}{" "}</th> })}
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map(function (row, idx) {
+              return (
+                <tr key={idx}>{row.map(function (cell, idx) {
+                  return (<td key={idx}>{cell}</td>);
+                })}</tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+}
 
 class PageNav extends Component {
   handleSelect(selectedKey) {
@@ -15,16 +58,44 @@ class PageNav extends Component {
   render() {
     return (
       <div>
-          <Nav bsStyle="tabs" justified activeKey={1} onSelect={this.handleSelect}>
-            <NavItem eventKey={1} href="/home">Home</NavItem>
-            <NavItem eventKey={2} title="Item">My Account</NavItem>
-            <NavItem eventKey={3} >Shopping Cart</NavItem>
-            <NavItem eventKey={4} >Sign up</NavItem>
-          </Nav>
+        <Nav bsStyle="tabs" justified activeKey={1} onSelect={this.handleSelect}>
+          <NavItem eventKey={1} href="/home">Home</NavItem>
+          <NavItem eventKey={2} title="Item">My Account</NavItem>
+          <NavItem eventKey={3} >Shopping Cart</NavItem>
+          <NavItem eventKey={4} >Sign up</NavItem>
+        </Nav>
       </div>
     );
   }
 }
+
+class Search extends Component {
+  submitSearch() {
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      start: -1,
+      end: -1,
+      key: ""
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Form componentClass="fieldset" inline justified>
+          <FormGroup bsSize="small">
+            <FormControl id="se0" type="text" placeholder="From" />
+            <FormControl id="se0" type="text" placeholder="To" />
+            <FormControl id="se1" type="text" placeholder="Keyword" />
+            {/* <Button onClick={this.submitSearch} type="submit">Submit</Button> */}
+          </FormGroup>
+        </Form>
+      </div>
+    );
+  }
+}
+
 
 class MyHeader extends Component {
   render() {
@@ -35,13 +106,7 @@ class MyHeader extends Component {
           <tr width={745}><div id="pn"><PageNav className="mypn" /></div></tr>
           <tr width={745}>
             <td height={70}>
-              <Form componentClass="fieldset" inline justified>
-                <FormGroup bsSize="small">
-                  <FormControl id="se0" type="text" placeholder="From" />
-                  <FormControl id="se0" type="text" placeholder="To" />
-                  <FormControl id="se1" type="text" placeholder="Keyword" />
-                </FormGroup>
-              </Form>
+              <Search />
             </td>
           </tr>
         </Table >
@@ -55,6 +120,7 @@ class App extends Component {
     return (
       <div className="App">
         <MyHeader />
+        <Excel headers={headers} initialData={data} />
         <div>{}</div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
